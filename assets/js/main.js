de5ff47b8d113f8a61ad38ca4c935d5ef0f446db
James McDougald <2smooventertainment@gmail.com>
2017-11-29 02:01:17 +0100

    added beginning to navigation bar.

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,14 @@ $(document).ready(function() {
 
 });
 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
 
 
 function setup_progress_bar_animation()
